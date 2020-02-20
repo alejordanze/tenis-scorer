24436aa9db9c6d95e2d8d77dac026fc30c77d604
Miguel Jordán <alejordan81097@gmail.com>
2020-02-20 09:50:20 -0400

    using constants for getLiteral and renaming isInAdvantage

--- src/TennisGame2.java
+++ src/TennisGame2.java
@@ -4,11 +4,13 @@ public class TennisGame2 implements TennisGame
     public int player1Points = 0;
     public int player2Points = 0;
     
-    public String player1Result = "";
-    public String player2Result = "";
     private String player1Name;
     private String player2Name;
 
+    final int _LOVE = 0;
+    final int _FIFTEEN = 1;
+    final int _THIRTY = 2;
+    final int _FORTY = 3;
     public TennisGame2(String player1Name, String player2Name) {
         this.player1Name = player1Name;
         this.player2Name = player2Name;
@@ -18,14 +20,14 @@ public class TennisGame2 implements TennisGame
     	
         String score = "";
     	if (isNormal())
-            score = getLiteral(player1Points) + "-" + getLiteral(player2Points);
+            score = getNormalScore();
         if (isTie())
             score = getLiteral(player1Points) + "-All";
 		if (isDeuce())
             score = "Deuce";
-		if (isInAdvantage(player1Points, player2Points))
+		if (isInAdvantageOver(player1Points, player2Points))
             score = "Advantage player1";
-        if (isInAdvantage(player2Points, player1Points))
+        if (isInAdvantageOver(player2Points, player1Points))
             score = "Advantage player2";    
     	if (isWinner(player1Points,player2Points))
             score = "Win for player1";
@@ -35,20 +37,23 @@ public class TennisGame2 implements TennisGame
         return score;
     }
 
+	private String getNormalScore() {
+		return getLiteral(player1Points) + "-" + getLiteral(player2Points);
+	}
+
 	private boolean isNormal() {
 		return player1Points != player2Points;
 	}
 
 	private String getLiteral(int p1point2) {
 		String literal = "";
-		
-		if (p1point2==0)
+		if (p1point2 == _LOVE)
 		    literal = "Love";
-		if (p1point2==1)
+		if (p1point2 == _FIFTEEN)
 		    literal = "Fifteen";
-		if (p1point2==2)
+		if (p1point2 == _THIRTY)
 		    literal = "Thirty";
-		if (p1point2==3)
+		if (p1point2 == _FORTY)
 		    literal = "Forty";
 		return literal;
 	}
@@ -57,7 +62,7 @@ public class TennisGame2 implements TennisGame
 		return player1Points>=4 && player2Points>=0 && (player1Points-player2Points)>=2;
 	}
 
-	private boolean isInAdvantage(int player1Points, int player2Points) {
+	private boolean isInAdvantageOver(int player1Points, int player2Points) {
 		return player1Points > player2Points && player2Points >= 3;
 	}
 
